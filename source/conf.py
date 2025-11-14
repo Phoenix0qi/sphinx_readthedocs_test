27f29d795436e658d22dd19e56e502984bc26862
chongqi00 <1757197756@qq.com>
2025-11-15 02:36:56 +0800

    11

--- source/conf.py
+++ source/conf.py
@@ -15,7 +15,7 @@ release = 'v0.0.1'
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
 extensions = [
-    'myst_parser',
+    'myst-parser',
     "sphinxcontrib.mermaid",
     "sphinx_copybutton",
 ]
